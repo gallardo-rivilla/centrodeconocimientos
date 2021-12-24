f29fa18fe391a073bba239db389da1032fc660e3
gallardo-rivilla <gallardo_rivilla@hotmail.com>
2021-12-24 12:05:29 +0100

    conexion MYSQL

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from Flask import from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,redirect,url_for,render_template,request
+from flaskext.mysql import MySQL
+from pymysql.cursors import Cursor
 
 app=Flask(__name__)
+
+#Conexion con BBDD MYSQL
+mysql =MySQL()
+app.config['MYSQL_DATABASE_HOST']= 'localhost'
+app.config['MYSQL_DATABASE_USER']= 'root'
+app.config['MYSQL_DATABASE_PASSWORD']= ''
+app.config['MYSQL_DATABASE_DB']= 'centroconocimiento'
+mysql.init_app(app)
+
+
+# Iniciamos app
 @app.route('/',methods=['GET','POST'])
 
 def home():
+    #Conexion a la MySQL
+    sql ="INSERT INTO `empleados` (`id`, `nombre`, `corrreo`, `foto`) VALUES (NULL, 'Pedro', 'pedro@atsistemas.com', 'foto2.jpeg');"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
     if request.method=='POST':
         # Handle POST Request here
         return render_template('empleados/index.html')
