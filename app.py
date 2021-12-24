02a5810c0c20d8237889b816b20f885b8f03cd40
gallardo-rivilla <gallardo_rivilla@hotmail.com>
2021-12-24 18:39:55 +0100

    dev

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,redirect,url_for,render_template,request, redirect, url_for
 from flaskext.mysql import MySQL
 from pymysql.cursors import Cursor
 
@@ -14,21 +14,44 @@ mysql.init_app(app)
 
 
 # Iniciamos app
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 
 def home():
     #Conexion a la MySQL
-    sql ="INSERT INTO `empleados` (`id`, `nombre`, `corrreo`, `foto`) VALUES (NULL, 'Pedro', 'pedro@atsistemas.com', 'foto2.jpeg');"
-    conn=mysql.connect()
-    cursor=conn.cursor()
-    cursor.execute(sql)
-    conn.commit()
+    # sql ="INSERT INTO `empleados` (`id`, `nombre`, `corrreo`, `foto`) VALUES (NULL, 'Pedro', 'pedro@atsistemas.com', 'foto2.jpeg');"
+    # conn=mysql.connect()
+    # cursor=conn.cursor()
+    # cursor.execute(sql)
+    # conn.commit()
 
     if request.method=='POST':
         # Handle POST Request here
         return render_template('empleados/index.html')
     return render_template('empleados/index.html')
 
+
+
+@app.route('/create')
+def create():
+   
+    return render_template('empleados/create.html',msg="Por favor inserta una foto")
+    
+@app.route('/store', methods=['POST'])
+def store():
+   _nombre=request.form['txtNombre']
+   _correo=request.form['txtCorreo']
+   _foto=request.files['txtFoto']
+   
+   sql ="INSERT INTO `empleados` (`id`, `nombre`, `corrreo`, `foto`) VALUES (NULL, %s, %s, %s);"
+   datos =(_nombre,_correo,_foto.filename)
+
+   conn=mysql.connect()
+   cursor=conn.cursor()
+   cursor.execute(sql,datos)
+   conn.commit()
+   return render_template('empleados/index.html')
+
+# Inicio de programa
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
